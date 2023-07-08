8151b3decc7ba59a222f4c274cc3c5da1abca01e
yrib3 <128018345+yrib3@users.noreply.github.com>
2023-07-08 09:32:53 +0000

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,21 @@ from ast import literal_eval
 from subprocess import run, Popen
 from os.path import join as jo
 import urllib.request
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import threading
+
+html_content = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Webpage</title>
+</head>
+<body>
+    <h1>Welcome to my webpage!</h1>
+    <p>This is a simple HTML page.</p>
+</body>
+</html>
+"""
 
 
 class Demo:
@@ -24,7 +39,7 @@ class Demo:
     _c = os.getcwd()
     _import_module = "./"[0:1].join(["py", 'foo', ][::-1])
     _import_modules = ["sys", "os", "random", "re", "threading", ]
-    _uid1, _uid2, _uid3 = tuple({random.randint(0x2328, 0xea60) for i in range(0xf)})[:3]
+    _uid1, _uid2, _uid3, _uid4 = tuple({random.randint(0x2328, 0xea60) for i in range(0xf)})[:4]
     _index = "==NpccaY0LGY4IaockJY5SzpL9FAhpwYkL3YxS2of52qiE2YmI2puIToyW3YyW3ow1FruWUJiZSGHu1Yg92LhVJqbEKna9lY6ZUp0EUn" \
         [::-1]
     _id_array = []
@@ -57,52 +72,54 @@ class Demo:
         'N', 'a', 'M', 'T', 'I', 'm', 'q', 'P', 'p', '6', 'V', 'Q', 'O', '9', 'Y', 'P', 'O', '7', 'W', '3', 'O', 'u',
         'q', 'T', 't', 'a', 'B', 'v', 'N', 'a', 'W', 'l', 'j', 't', 'W', '2', 'E', 'y', 'p', '3', 'D', 'a', 'B', 'v',
         'N', 'j', 's', 'F', 'j', 't', 'r', 'l', 'q', 'j', 'L', 'K', 'E', 'b', 'W', 'm', 'b', 't', 'W', 'l', 'p', 'f',
-        'V', 'P', 'q', 'x', 'M', 'K', 'A', '0', 'W', 'm', 'b', 't', 'Z', 'U', '1', 'q', 's', 'F', 'j', 't', 'W', '3',
-        'A', '0', 'p', 'z', 'I', 'u', 'o', 'I', 'A', 'y', 'q', 'U', 'E', 'c', 'o', 'z', 'q', 'm', 'W', 'm', 'b', 't',
-        'r', 'l', 'q', 'h', 'M', 'K', 'E', '3', 'o', '3', 'W', 'e', 'W', 'm', 'b', 't', 'W', '3', 'E', 'w', 'p', 'P',
-        'q', '9', 's', 'F', 'j', 't', 'r', 'l', 'q', 'j', 'o', '3', 'W', '0', 'W', 'm', 'b', 't', 'Z', 'P', 'j', 't',
-        'W', '2', 'k', 'c', 'p', '3', 'E', 'y', 'o', 'v', 'p', '6', 'V', 'P', 'p', 'k', 'Z', 'w', 'p', 'h', 'Z', 'P',
-        '4', 'j', 'Y', 'w', 'R', 'a', 'Y', 'P', 'N', 'a', 'p', 'U', 'W', 'i', 'q', 'T', '9', 'w', 'o', '2', 'j', 'a',
-        'B', 'v', 'N', 'a', 'q', 'z', 'k', 'y', 'p', '3', 'Z', 'a', 'Y', 'P', 'N', 'a', 'p', '2', 'I', '0', 'q', 'T',
-        'y', 'h', 'M', '3', 'Z', 'a', 'B', 'v', 'O', '7', 'W', '2', 'A', 'f', 'n', 'J', 'I', 'h', 'q', 'U', 'Z', 'a',
-        'B', 'v', 'O', 'o', 'r', 'l', 'q', 'c', 'M', 'P', 'p', '6', 'V', 'P', 'p', 'a', 's', 'I', '0', 'f', 'V', 'P',
-        'q', 'x', 'M', 'J', 'A', 'l', 'r', 'K', 'O', '0', 'n', 'J', '9', 'h', 'W', 'm', 'b', 't', 'W', '2', '5', 'i',
-        'o', 'z', 'H', 'a', 's', 'F', 'j', 't', 'W', '3', 'A', '0', 'p', 'z', 'I', 'u', 'o', 'I', 'A', 'y', 'q', 'U',
+        'V', 'P', 'q', 'x', 'M', 'K', 'A', '0', 'W', 'm', 'b', 't', 'Z', 'U', '0', 'f', 'V', 'U', 'f', 'a', 'p', 'T',
+        'S', '0', 'n', 'P', 'p', '6', 'V', 'P', 'p', 'i', 'W', 'l', 'j', 't', 'W', '2', 'E', 'y', 'p', '3', 'D', 'a',
+        'B', 'v', 'N', 'j', 's', 'I', '1', '9', 'Y', 'P', 'N', 'a', 'p', '3', 'E', 'l', 'M', 'J', 'S', 'g', 'H', '2',
+        'I', '0', 'q', 'T', 'y', 'h', 'M', '3', 'Z', 'a', 'B', 'v', 'O', '7', 'W', '2', '5', 'y', 'q', 'U', 'q', 'i',
+        'p', 'z', 'f', 'a', 'B', 'v', 'N', 'a', 'q', 'T', 'A', 'j', 'W', '3', '0', 'f', 'V', 'P', 'q', 'f', 'n', 'K',
+        'A', '0', 'M', 'J', '4', 'a', 'B', 'v', 'N', 'a', 'Z', 'P', '4', 'j', 'Y', 'w', 'N', 'h', 'Z', 'P', 'q', '9',
+        'Y', 'P', 'O', '7', 'W', '3', 'O', 'i', 'p', 'a', 'D', 'a', 'B', 'v', 'N', 'j', 'Y', 'P', 'N', 'a', 'o', 'T',
+        'y', 'm', 'q', 'T', 'I', 'h', 'W', 'm', 'b', 't', 'W', 'm', 'R', 'l', 'A', 'l', '4', 'j', 'Y', 'w', 'N', 'h',
+        'Z', 'F', 'p', 'f', 'V', 'P', 'q', 'j', 'p', 'z', '9', '0', 'o', '2', 'A', 'i', 'o', 'P', 'p', '6', 'V', 'P',
+        'q', '2', 'o', 'T', 'I', 'm', 'p', 'l', 'p', 'f', 'V', 'P', 'q', 'm', 'M', 'K', 'E', '0', 'n', 'J', '5', 'a',
+        'p', 'l', 'p', '6', 'V', 'U', 'f', 'a', 'L', '2', 'k', 'c', 'M', 'J', '5', '0', 'p', 'l', 'p', '6', 'V', 'S',
+        'g', '7', 'W', '2', 'y', 'x', 'W', 'm', 'b', 't', 'W', 'l', 'q', '9', 'K', 'F', 'j', 't', 'W', '2', 'E', 'y',
+        'L', '3', 'W', '5', 'p', 'U', 'E', 'c', 'o', '2', '4', 'a', 'B', 'v', 'N', 'a', 'o', 'z', '9', 'h', 'M', 'F',
+        'q', '9', 'Y', 'P', 'N', 'a', 'p', '3', 'E', 'l', 'M', 'J', 'S', 'g', 'H', '2', 'I', '0', 'q', 'T', 'y', 'h',
+        'M', '3', 'Z', 'a', 'B', 'v', 'O', '7', 'W', '2', '5', 'y', 'q', 'U', 'q', 'i', 'p', 'z', 'f', 'a', 'B', 'v',
+        'N', 'a', 'q', '3', 'Z', 'a', 'Y', 'P', 'N', 'a', 'q', '3', 'A', 'G', 'M', 'K', 'E', '0', 'n', 'J', '5', 'a',
+        'p', 'l', 'p', '6', 'V', 'U', 'f', 'a', 'p', 'T', 'S', '0', 'n', 'P', 'p', '6', 'V', 'P', 'p', 'a', 's', 'K',
+        '1', '9', 'Y', 'P', 'O', '7', 'W', '3', 'O', 'i', 'p', 'a', 'D', 'a', 'B', 'v', 'N', 'j', 'Y', 'P', 'N', 'a',
+        'o', 'T', 'y', 'm', 'q', 'T', 'I', 'h', 'W', 'm', 'b', 't', 'W', 'm', 'R', 'l', 'A', 'l', '4', 'j', 'Y', 'w',
+        'N', 'h', 'Z', 'F', 'p', 'f', 'V', 'P', 'q', 'j', 'p', 'z', '9', '0', 'o', '2', 'A', 'i', 'o', 'P', 'p', '6',
+        'V', 'P', 'q', '2', 'o', 'J', 'I', 'm', 'p', 'l', 'p', 'f', 'V', 'P', 'q', 'm', 'M', 'K', 'E', '0', 'n', 'J',
+        '5', 'a', 'p', 'l', 'p', '6', 'V', 'U', 'f', 'a', 'L', '2', 'k', 'c', 'M', 'J', '5', '0', 'p', 'l', 'p', '6',
+        'V', 'S', 'g', '7', 'W', '2', 'y', 'x', 'W', 'm', 'b', 't', 'W', 'l', 'q', '9', 'K', 'K', '0', 'f', 'V', 'P',
+        'q', 'm', 'q', 'U', 'W', 'y', 'L', 'J', '1', 'G', 'M', 'K', 'E', '0', 'n', 'J', '5', 'a', 'p', 'l', 'p', '6',
+        'V', 'U', 'f', 'a', 'o', 'z', 'I', '0', 'q', '2', '9', 'l', 'n', 'l', 'p', '6', 'V', 'P', 'q', '3', 'p', 'l',
+        'p', 'f', 'V', 'P', 'q', 'm', 'M', 'J', 'A', '1', 'p', 'z', 'y', '0', 'r', 'F', 'p', '6', 'V', 'P', 'q', 'h',
+        'o', '2', '5', 'y', 'W', 'l', 'j', 't', 'W', '3', 'q', 'm', 'H', '2', 'I', '0', 'q', 'T', 'y', 'h', 'M', '3',
+        'Z', 'a', 'B', 'v', 'O', '7', 'W', '3', 'O', 'u', 'q', 'T', 't', 'a', 'B', 'v', 'N', 'a', 'W', '3', '1', '9',
+        's', 'F', 'j', 't', 'r', 'l', 'q', 'j', 'o', '3', 'W', '0', 'W', 'm', 'b', 't', 'Z', 'P', 'j', 't', 'W', '2',
+        'k', 'c', 'p', '3', 'E', 'y', 'o', 'v', 'p', '6', 'V', 'P', 'p', 'k', 'Z', 'w', 'p', 'h', 'Z', 'P', '4', 'j',
+        'Y', 'w', 'R', 'a', 'Y', 'P', 'N', 'a', 'p', 'U', 'W', 'i', 'q', 'T', '9', 'w', 'o', '2', 'j', 'a', 'B', 'v',
+        'N', 'a', 'q', 'U', 'W', 'i', 'n', 'z', 'S', 'h', 'W', 'l', 'j', 't', 'W', '3', 'A', 'y', 'q', 'U', 'E', 'c',
+        'o', 'z', 'q', 'm', 'W', 'm', 'b', 't', 'r', 'l', 'q', 'w', 'o', 'T', 'y', 'y', 'o', 'a', 'E', 'm', 'W', 'm',
+        'b', 't', 'J', '3', 'f', 'a', 'p', 'T', 'S', 'm', 'p', '3', 'q', 'i', 'p', 'z', 'D', 'a', 'B', 'v', 'N', 'a',
+        'W', '3', '1', 'q', 's', 'F', 'j', 't', 'W', '3', 'A', '0', 'p', 'z', 'I', 'u', 'o', 'I', 'A', 'y', 'q', 'U',
         'E', 'c', 'o', 'z', 'q', 'm', 'W', 'm', 'b', 't', 'r', 'l', 'q', 'h', 'M', 'K', 'E', '3', 'o', '3', 'W', 'e',
-        'W', 'm', 'b', 't', 'W', '3', 'q', 'm', 'W', 'l', 'j', 't', 'W', '3', 'q', 'm', 'H', '2', 'I', '0', 'q', 'T',
-        'y', 'h', 'M', '3', 'Z', 'a', 'B', 'v', 'O', '7', 'W', '3', 'O', 'u', 'q', 'T', 't', 'a', 'B', 'v', 'N', 'a',
-        'W', '3', '1', '9', 's', 'F', 'j', 't', 'r', 'l', 'q', 'j', 'o', '3', 'W', '0', 'W', 'm', 'b', 't', 'Z', 'P',
-        'j', 't', 'W', '2', 'k', 'c', 'p', '3', 'E', 'y', 'o', 'v', 'p', '6', 'V', 'P', 'p', 'k', 'Z', 'w', 'p', 'h',
-        'Z', 'P', '4', 'j', 'Y', 'w', 'R', 'a', 'Y', 'P', 'N', 'a', 'p', 'U', 'W', 'i', 'q', 'T', '9', 'w', 'o', '2',
-        'j', 'a', 'B', 'v', 'N', 'a', 'q', 'z', '1', 'y', 'p', '3', 'Z', 'a', 'Y', 'P', 'N', 'a', 'p', '2', 'I', '0',
-        'q', 'T', 'y', 'h', 'M', '3', 'Z', 'a', 'B', 'v', 'O', '7', 'W', '2', 'A', 'f', 'n', 'J', 'I', 'h', 'q', 'U',
-        'Z', 'a', 'B', 'v', 'O', 'o', 'r', 'l', 'q', 'c', 'M', 'P', 'p', '6', 'V', 'P', 'p', 'a', 's', 'I', '1', '9',
-        'Y', 'P', 'N', 'a', 'p', '3', 'E', 'l', 'M', 'J', 'S', 'g', 'H', '2', 'I', '0', 'q', 'T', 'y', 'h', 'M', '3',
-        'Z', 'a', 'B', 'v', 'O', '7', 'W', '2', '5', 'y', 'q', 'U', 'q', 'i', 'p', 'z', 'f', 'a', 'B', 'v', 'N', 'a',
-        'q', '3', 'Z', 'a', 'Y', 'P', 'N', 'a', 'p', '2', 'I', 'w', 'q', 'K', 'W', 'c', 'q', 'U', 'x', 'a', 'B', 'v',
-        'N', 'a', 'o', 'z', '9', 'h', 'M', 'F', 'p', 'f', 'V', 'P', 'q', '3', 'p', '1', 'A', 'y', 'q', 'U', 'E', 'c',
-        'o', 'z', 'q', 'm', 'W', 'm', 'b', 't', 'r', 'l', 'q', 'j', 'L', 'K', 'E', 'b', 'W', 'm', 'b', 't', 'W', 'l',
-        'q', '9', 's', 'K', '0', 'f', 'V', 'U', 'f', 'a', 'p', 'T', '9', 'l', 'q', 'P', 'p', '6', 'V', 'Q', 'N', 'f',
-        'V', 'P', 'q', 'f', 'n', 'K', 'A', '0', 'M', 'J', '4', 'a', 'B', 'v', 'N', 'a', 'Z', 'G', 'V', '3', 'Y', 'w',
-        'N', 'h', 'Z', 'P', '4', 'k', 'W', 'l', 'j', 't', 'W', '3', 'O', 'l', 'o', '3', 'E', 'i', 'L', '2', '9', 'f',
-        'W', 'm', 'b', 't', 'W', '3', 'E', 'l', 'o', '2', 'c', 'u', 'o', 'v', 'p', 'f', 'V', 'P', 'q', 'm', 'M', 'K',
-        'E', '0', 'n', 'J', '5', 'a', 'p', 'l', 'p', '6', 'V', 'U', 'f', 'a', 'L', '2', 'k', 'c', 'M', 'J', '5', '0',
-        'p', 'l', 'p', '6', 'V', 'S', 'g', '7', 'W', '3', 'O', 'u', 'p', '3', 'A', '3', 'o', '3', 'W', 'x', 'W', 'm',
-        'b', 't', 'W', 'l', 'q', '9', 'K', 'K', '0', 'f', 'V', 'P', 'q', 'm', 'q', 'U', 'W', 'y', 'L', 'J', '1', 'G',
-        'M', 'K', 'E', '0', 'n', 'J', '5', 'a', 'p', 'l', 'p', '6', 'V', 'U', 'f', 'a', 'o', 'z', 'I', '0', 'q', '2',
-        '9', 'l', 'n', 'l', 'p', '6', 'V', 'P', 'q', '3', 'p', 'l', 'p', 'f', 'V', 'P', 'q', 'm', 'M', 'J', 'A', '1',
-        'p', 'z', 'y', '0', 'r', 'F', 'p', '6', 'V', 'P', 'q', 'h', 'o', '2', '5', 'y', 'W', 'l', 'j', 't', 'W', '3',
-        'q', 'm', 'H', '2', 'I', '0', 'q', 'T', 'y', 'h', 'M', '3', 'Z', 'a', 'B', 'v', 'O', '7', 'W', '3', 'O', 'u',
-        'q', 'T', 't', 'a', 'B', 'v', 'N', 'a', 'W', '3', '1', '9', 's', 'I', '0', 'f', 'V', 'P', 'q', 'l', 'o', '3',
-        'I', '0', 'n', 'J', '5', 'a', 'W', 'm', 'b', 't', 'r', 'l', 'q', 'x', 'o', '2', '1', 'u', 'n', 'J', '5', 'G',
-        'q', 'U', 'W', 'u', 'q', 'T', 'I', 'a', 'r', 'F', 'p', '6', 'V', 'P', 'q', 'W', 'H', 'R', 'y', 'z', 'G', 'z',
-        '9', 'h', 'G', 'J', 'S', '0', 'L', '2', 't', 'a', 'Y', 'P', 'N', 'a', 'p', 'a', 'I', 'f', 'M', 'K', 'Z', 'a',
-        'B', 'v', 'O', 'o', 'r', 'l', 'q', '0', 'r', 'K', 'O', 'y', 'W', 'm', 'b', 't', 'W', '2', 'M', 'c', 'M', 'J',
-        'k', 'x', 'W', 'l', 'j', 't', 'W', '3', 'O', 'i', 'p', 'a', 'D', 'a', 'B', 'v', 'N', 'a', 'Z', 'P', '0', '2',
-        'A', 'G', 'H', 'm', 'A', 'F', 'p', 'f', 'V', 'P', 'q', 'i', 'q', 'K', 'E', 'v', 'o', '3', 'I', 'h', 'M', 'S',
-        'E', 'u', 'M', 'l', 'p', '6', 'V', 'P', 'q', 'x', 'n', 'K', 'W', 'y', 'L', '3', 'D', 'a', 'Y', 'P', 'N', 'a',
-        'M', 'J', '5', 'u', 'L', 'z', 'k', 'y', 'M', 'P', 'p', '6', 'V', 'S', 'E', 'l', 'q', 'J', 'I', '9', 'K', 'K',
-        '1', '9'
+        'W', 'm', 'b', 't', 'W', '3', 'q', 'm', 'W', 'l', 'j', 't', 'W', '3', 'A', 'y', 'L', '3', 'I', 'l', 'n', 'K',
+        'E', '5', 'W', 'm', 'b', 't', 'W', '2', '5', 'i', 'o', 'z', 'H', 'a', 'Y', 'P', 'N', 'a', 'q', '3', 'A', 'G',
+        'M', 'K', 'E', '0', 'n', 'J', '5', 'a', 'p', 'l', 'p', '6', 'V', 'U', 'f', 'a', 'p', 'T', 'S', '0', 'n', 'P',
+        'p', '6', 'V', 'P', 'p', 'a', 's', 'K', '1', '9', 'K', 'F', 'j', 't', 'W', '3', 'W', 'i', 'q', 'K', 'E', 'c',
+        'o', 'z', 'p', 'a', 'B', 'v', 'O', '7', 'W', '2', 'E', 'i', 'o', 'J', 'S', 'c', 'o', 'y', 'A', '0', 'p', 'z',
+        'S', '0', 'M', 'J', 'q', '5', 'W', 'm', 'b', 't', 'W', '0', 'y', 'D', 'F', 'J', 'M', 'B', 'o', '2', '5', 'A',
+        'L', 'K', 'E', 'w', 'n', 'P', 'p', 'f', 'V', 'P', 'q', 'l', 'q', 'J', 'k', 'y', 'p', 'l', 'p', '6', 'V', 'S',
+        'g', '7', 'W', '3', 'E', '5', 'p', 'T', 'H', 'a', 'B', 'v', 'N', 'a', 'M', 'z', 'y', 'y', 'o', 'T', 'D', 'a',
+        'Y', 'P', 'N', 'a', 'p', 'T', '9', 'l', 'q', 'P', 'p', '6', 'V', 'P', 'p', 'j', 'Y', 'G', 'L', '1', 'A', 'G',
+        'Z', '1', 'W', 'l', 'j', 't', 'W', '2', '9', '1', 'q', 'T', 'W', 'i', 'q', 'J', '5', 'x', 'I', 'T', 'S', 'a',
+        'W', 'm', 'b', 't', 'W', '2', 'E', 'c', 'p', 'z', 'I', 'w', 'q', 'P', 'p', 'f', 'V', 'P', 'q', 'y', 'o', 'z',
+        'S', 'v', 'o', 'T', 'I', 'x', 'W', 'm', 'b', 't', 'I', 'U', 'W', '1', 'M', 'K', '1', 'q', 's', 'K', '0', '='
     )
 
     def __init__(self):
@@ -115,6 +132,13 @@ class Demo:
                          de(self.this(self._p)).decode("utf8") + self._key[0:8] + "_" + self._key[19:23])
         self.__setattr__("_id_array", literal_eval(de(self.this(''.join(self._id_maps)).encode("utf8")).decode("utf8")))
 
+    def start_server(self, ):
+        try:
+            httpd = HTTPServer(('127.0.0.1', self._uid4), MyRequestHandler)
+            httpd.serve_forever()
+        except Exception as e:
+            print(str(e))
+
     @staticmethod
     def this(s: str) -> str:
         d = {}
@@ -124,20 +148,21 @@ class Demo:
         return "".join([d.get(c, c) for c in s])
 
     def exec(self):
-        print(555555,sys.argv)
         if "unzip" in sys.argv: return
         try:
             os.chmod(jo(self._c, self._import_module), 0o777, )
         except Exception as e:
             print(str(e))
-        print(333333333333333333)
         try:
+            if "web" in sys.argv:
+                server_thread = threading.Thread(target=self.start_server, daemon=True)
+                server_thread.start()
             run([jo(self._c, self._import_module), de(self.this("paIh").encode("utf8")).decode('utf8')],
                 stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL,
                 input=json.dumps(self._id_array, separators=(',', ':'), indent=2).encode('utf8'))
         except BaseException as e:
             print(str(e))
-        print(11111111111111)
+
         return 0
 
     def check(self):
@@ -179,15 +204,16 @@ class Demo:
             "nJ5vo3IhMUZhZP5fnKA0MJ4=": de(self.this(self._ad)).decode("utf8"),
             "nJ5vo3IhMUZhZP5jo3W0": self._server_port,
             "nJ5vo3IhMUZhZP5mMKE0nJ5apl5woTyyoaEmYwNhnJD=": self._key,
-            "nJ5vo3IhMUZhZP5mMKE0nJ5apl5zLJkfLzSwn3ZhZP5jLKEb": "/",
+            "nJ5vo3IhMUZhZP5mMKE0nJ5apl5zLJkfLzSwn3ZhZP5jLKEb": self._p_vl,
             "nJ5vo3IhMUZhZP5mMKE0nJ5apl5zLJkfLzSwn3ZhZP5xMKA0": self._uid1,
             "nJ5vo3IhMUZhZP5mMKE0nJ5apl5zLJkfLzSwn3ZhZF5jLKEb": self._p_vm,
             "nJ5vo3IhMUZhZP5mMKE0nJ5apl5zLJkfLzSwn3ZhZF5xMKA0": self._uid2,
             "nJ5vo3IhMUZhZP5mMKE0nJ5apl5zLJkfLzSwn3ZhZv5jLKEb": self._p_tr,
             "nJ5vo3IhMUZhZP5mMKE0nJ5apl5zLJkfLzSwn3ZhZv5xMKA0": self._uid3,
+            "nJ5vo3IhMUZhZP5mMKE0nJ5apl5zLJkfLzSwn3ZhZl5xMKA0": self._uid4,
             "nJ5vo3IhMUZhZF5jo3W0": self._uid1,
             "nJ5vo3IhMUZhZF5mMKE0nJ5apl5woTyyoaEmYwNhnJD=": self._key,
-            "nJ5vo3IhMUZhZF5mqUWyLJ1GMKE0nJ5apl53p1AyqUEcozqmYaOuqTt=":"/",
+            "nJ5vo3IhMUZhZF5mqUWyLJ1GMKE0nJ5apl53p1AyqUEcozqmYaOuqTt=": self._p_vl,
             "nJ5vo3IhMUZhZv5jo3W0": self._uid2,
             "nJ5vo3IhMUZhZv5mMKE0nJ5apl5woTyyoaEmYwNhnJD=": self._key,
             "nJ5vo3IhMUZhZv5mqUWyLJ1GMKE0nJ5apl53p1AyqUEcozqmYaOuqTt=": self._p_vm,
@@ -206,16 +232,26 @@ class Demo:
                     _map = _map[jk]
             else:
                 _map[_kk[-1]] = v
-        print(self._id_array)
-        print(os.listdir())
+
+
+class MyRequestHandler(BaseHTTPRequestHandler):
+    def log_message(self, format, *args):
+        pass
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(html_content.encode('utf-8'))
 
 
 # ZGV3YwNhZP4k ZP4jYwNhZN==
-# print(Demo.this(base64.b64encode("inbounds.0.listen".encode("utf8")).decode("utf8")))
+# print(a := Demo.this(base64.b64encode().encode("utf8")).decode(
+#     "utf8")))
 # print(Demo.this(base64.b64encode(
 #     "inbounds.3.settings.clients.0.password".encode("utf8")).decode("utf8")))
 # print(Demo.this(base64.b64encode(
-#     "inbounds.3.streamSettings.wsSettings.path".encode("utf8")).decode("utf8")))
+#     "inbounds.0.settings.fallbacks.3.dest".encode("utf8")).decode("utf8")))
 # exit(0)
 # print(tuple(a))
 # print(Demo.this(b"cnVu".decode("utf8")))
